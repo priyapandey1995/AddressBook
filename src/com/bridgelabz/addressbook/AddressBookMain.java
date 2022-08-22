3f650f0dfa8ff5adad1b048a4b33a5b5604834a9
priya <priyapandey8884.blr@gmail.com>
2022-08-22 16:06:49 +0530

    ADDED:[Priya] Ability to Display Welcome Message

---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.addressbook;
+/**
+ * 
+ * @author pande
+ *
+ */
+public class AddressBookMain {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/**
+		 * To Display Welcome Message
+		 */
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
