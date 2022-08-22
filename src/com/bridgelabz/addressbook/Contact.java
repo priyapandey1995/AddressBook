2d8228f08d566e9800915f3350076df696bbcdf0
priya <priyapandey8884.blr@gmail.com>
2022-08-23 00:21:18 +0530

    ADDED:[Priya] Ability to create contact in Address Book

--- src/com/bridgelabz/addressbook/Contact.java
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -1,5 +1,66 @@
 package com.bridgelabz.addressbook;
 
 public class Contact {
+	
+	/**
+	 * Initially,declaring variables and marked them as private.
+	 * For each variables implementing getter and setter method
+	 * setter():is to assign values to variables
+	 * getter():is to get values from the variables
+	 */
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String zip;
+	private String phoneNo;
+	private String email;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
